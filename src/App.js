e0a1c4977f468107c3f2ad0021b0c82fcb449bda
rakeshjain1111 <rakeshkjain222@gmail.com>
2024-01-15 23:09:31 +0530

    set counter , style change done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState } from 'react'
 
 function App() {
+  const stylechange = {
+      title : "Rakesh Kumar Jain",
+      color : "Red",
+      fontSize : 21+"px"
+  }
+
+
+
+  const [change,update] =useState("CNC Web World");
+  const[count,setCount] =useState(5);
+  const[sty,updateStyle]=useState(stylechange);
+
+  function newStyle(){
+    updateStyle({
+      title : "Rakesh Kumar Jain",
+      color : "blue",
+      fontSize : 40+"px"
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>{change}</h1>
+      <button onClick={()=>update("Programming Site")}>Click Me</button>
+      <br/><hr/>
+ 
+      <h1>Counter</h1>
+      <h2>Value : {count} </h2>
+      <button onClick={()=>setCount(count-1)}>Subtract</button>
+      <button onClick={()=>setCount(count+1)}>Add</button>
+      <br/><hr/>
+      <h1>Style Changer</h1>
+      <h2 style={{color:sty.color,fontSize:sty.fontSize}}>{sty.title}</h2>
+      <button onClick={newStyle}>Change Style</button>
+
+    </>
+  )
 }
 
-export default App;
+export default App
