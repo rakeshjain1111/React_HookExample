aa87cb1036f7555f912dc2225af14e5ef5e8160c
rakeshjain1111 <rakeshkjain222@gmail.com>
2024-01-17 17:02:19 +0530

    some example added

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ function App() {
   const [change,update] =useState("CNC Web World");
   const[count,setCount] =useState(5);
   const[sty,updateStyle]=useState(stylechange);
-
+  const[fsize,setfsize]=useState('50px');
+  const[tcolor,setcolor]=useState('blue');
+  const[bgcolor,setbgcolor]= useState("#fff");
   function newStyle(){
     updateStyle({
       title : "Rakesh Kumar Jain",
@@ -22,8 +24,38 @@ function App() {
     });
   }
 
+  function getColor(e){
+    let value= e.target.value;
+    setcolor(value);
+  }
+
+  
+ 
+  function getValue(e){
+    let value=e.target.value+'px';
+    console.log(value);
+    setfsize(value);
+  }
+
+  function BgRed(){
+    setbgcolor('red');
+  }
+  function BgYellow(){
+    setbgcolor("yellow");
+  }
+  function BgBlue(){
+    setbgcolor("blue");
+  }
+  function BgGreen(){
+    setbgcolor("green");
+  }
+
+  function BgColor(e){
+    let value= e.target.value;
+    setbgcolor(value);
+  }
   return (
-    <>
+    <div style={{background:bgcolor}}>
       <h1>{change}</h1>
       <button onClick={()=>update("Programming Site")}>Click Me</button>
       <br/><hr/>
@@ -37,7 +69,29 @@ function App() {
       <h2 style={{color:sty.color,fontSize:sty.fontSize}}>{sty.title}</h2>
       <button onClick={newStyle}>Change Style</button>
       <hr/>
-    </>
+
+      <h1 style={{textAlign:'center'}}>Change FontSize</h1>
+      <h2 style={{fontSize:fsize,color:tcolor}} >Rakesh Kumar Jain</h2>
+      <label>Change Font Size:</label>
+      <input type='range' name='range' step={5} onChange={getValue}/><br/>
+      <label>Change Text Color:</label>
+      <input type='color' name='color' onChange={getColor} />
+      <hr/>
+      
+      <h1 style={{textAlign:'center'}}>Change BG Color </h1>
+      <div style={{alignItems:'center',display:'flex', justifyContent:'space-evenly'}}>
+          <button onClick={BgRed} style={{background:'red', color:'#fff', padding:"20px", border:"2px solid #fff"}}>Red</button>
+          <button onClick={BgYellow} style={{background:'yellow', color:'#000', padding:"20px", border:"2px solid #fff"}}>Yellow</button>
+          <button onClick={BgBlue} style={{background:'blue', color:'#fff', padding:"20px", border:"2px solid #fff"}}>Blue</button>
+          <button onClick={BgGreen} style={{background:'green', color:'#fff', padding:"20px", border:"2px solid #fff"}}>Green</button>
+         <span>Select BG Corrul</span> <input type='color' onChange={BgColor}/>
+      </div>
+      <hr/>
+      <div>
+        <h1 style={{textAlign:'center'}}>Change BG Color </h1>
+      </div>
+      
+    </div>
   )
 }
 
