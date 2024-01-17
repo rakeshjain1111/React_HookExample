f2528373cc0846c29972e873b70874dc47bd4e34
rakeshjain1111 <rakeshkjain222@gmail.com>
2024-01-17 17:21:53 +0530

    some updated

--- src/App.js
+++ src/App.js
@@ -56,40 +56,43 @@ function App() {
   }
   return (
     <div style={{background:bgcolor}}>
-      <h1>{change}</h1>
-      <button onClick={()=>update("Programming Site")}>Click Me</button>
+      <div style={{alignItems:'center', textAlign:'center'}}>
+          <h1>{change}</h1>
+          <button onClick={()=>update("Programming Site")}>Click Me</button>
       <br/><hr/>
- 
-      <h1>Counter</h1>
-      <h2>Value : {count} </h2>
-      <button onClick={()=>setCount(count-1)}>Subtract</button>
-      <button onClick={()=>setCount(count+1)}>Add</button>
-      <br/><hr/>
-      <h1>Style Changer</h1>
-      <h2 style={{color:sty.color,fontSize:sty.fontSize}}>{sty.title}</h2>
-      <button onClick={newStyle}>Change Style</button>
-      <hr/>
-
-      <h1 style={{textAlign:'center'}}>Change FontSize</h1>
-      <h2 style={{fontSize:fsize,color:tcolor}} >Rakesh Kumar Jain</h2>
-      <label>Change Font Size:</label>
-      <input type='range' name='range' step={5} onChange={getValue}/><br/>
-      <label>Change Text Color:</label>
-      <input type='color' name='color' onChange={getColor} />
+      </div>
+      <div style={{alignItems:'center',textAlign:'center'}}>
+          <h1>Counter</h1>
+          <h2>Value : {count} </h2>
+          <button onClick={()=>setCount(count-1)}>Subtract</button>
+          <button onClick={()=>setCount(count+1)}>Add</button>
+      </div>
+          <hr/>
+      <div style={{alignItems:'center',textAlign:'center'}}>
+          <h1>Style Changer</h1>
+          <h2 style={{color:sty.color,fontSize:sty.fontSize}}>{sty.title}</h2>
+          <button onClick={newStyle}>Change Style</button>
+      </div>
+          <hr/>
+      <div style={{alignItems:'center',textAlign:'center'}}>
+          <h1 style={{textAlign:'center'}}>Change FontSize</h1>
+          <h2 style={{fontSize:fsize,color:tcolor}} >Rakesh Kumar Jain</h2>
+          <label>Change Font Size:</label>
+          <input type='range' name='range' step={5} onChange={getValue}/><span>{fsize}</span><br/>
+          <label>Change Text Color:</label>
+          <input type='color' name='color' onChange={getColor} />
+      </div>
       <hr/>
       
-      <h1 style={{textAlign:'center'}}>Change BG Color </h1>
+      <h1 style={{textAlign:'center',textAlign:'center'}}>Change BG Color </h1>
       <div style={{alignItems:'center',display:'flex', justifyContent:'space-evenly'}}>
           <button onClick={BgRed} style={{background:'red', color:'#fff', padding:"20px", border:"2px solid #fff"}}>Red</button>
           <button onClick={BgYellow} style={{background:'yellow', color:'#000', padding:"20px", border:"2px solid #fff"}}>Yellow</button>
           <button onClick={BgBlue} style={{background:'blue', color:'#fff', padding:"20px", border:"2px solid #fff"}}>Blue</button>
           <button onClick={BgGreen} style={{background:'green', color:'#fff', padding:"20px", border:"2px solid #fff"}}>Green</button>
-         <span>Select BG Corrul</span> <input type='color' onChange={BgColor}/>
+         <span>Select BG Color</span> <input type='color' onChange={BgColor}/>
       </div>
       <hr/>
-      <div>
-        <h1 style={{textAlign:'center'}}>Change BG Color </h1>
-      </div>
       
     </div>
   )
